2db83a0593fc0745a6f645192e5dbacad51fc596
vaba2660 <v.balabanov85@gmail.com>
2019-09-12 15:17:47 +0300

    initial commit, 1st lesson ended

---
+++ cms/config.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors', true);
+date_default_timezone_set('Europe/Moscow');
+define('mysql:host=localhost; dbname=ruseller_cms');
+define('DB_USERNAME', 'user1');
+define('DB_PASSWORD', 'Vkshmuk:0707');
+define('CLASS_PATH', 'classes');
+define('TEMPLATE_PATH', 'templates');
+define('HOME_PAGE_NUM_ARTICLES', 5);
+define('ADMIN_USERNAME', 'admin');
+define('ADMIN_PASSWORD', 'admin');
+require(CLASS_PATH ."/article.php");
+
+function handleException($exception) {
+  echo '<p>Sorry, a problem occured. Please try later</p>';
+}
+
+set_exception_handler('handleException');
+?>
