ab5326783632356d3f2cfd0165387aaeb4b54613
vaba2660 <v.balabanov85@gmail.com>
2019-09-21 19:07:03 +0300

    downloaded files from ftp

---
+++ templates/default/include/footer.php
@@ -0,0 +1,6 @@
+<div id="footer">
+  Widget News &copy; 2019. All rights reserved. <a href="admin.php">Site admin panel</a>
+</div>
+</div>
+</body>
+</html>
---
+++ templates/default/include/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php echo htmlspecialchars($results['pageTitle']) ?></title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="container">
+    <a href=".">
+      <img src="images/logo.jpg" id="logo" alt="Widget News logo">
+    </a>
+  </div>
+</body>
+</html>
